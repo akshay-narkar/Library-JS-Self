e6a9a87208bcc8b95ffb14ecd26cc112182189af
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-01 20:57:39 +0530

    Refactor code using factor functions and module pattern

--- libscript.js
+++ libscript.js
@@ -5,7 +5,63 @@ const formBtn = document.querySelector('#form-btn');
 const forminput = document.querySelectorAll('input');
 let books = []
 
-function displayForm() {
+// const Person = (name) => {
+//   const sayName = () => console.log(`my name is ${name}`)
+//   return {sayName}
+// }
+
+// const Nerd = (name) => {
+//   // simply create a person and pull out the sayName function with destructuring assignment syntax!
+//   const {sayName} = Person(name)
+//   const doSomethingNerdy = () => console.log('nerd stuff')
+//   return {sayName, doSomethingNerdy}
+// }
+
+// const jeff = Nerd('jeff')
+
+// jeff.sayName() //my name is jeff
+// jeff.doSomethingNerdy() // nerd stuff
+
+
+//modular pattern
+
+const moduledisplay = (() => {
+
+  
+      const displaybooks = () => {  
+        
+                const table = document.querySelector('#tbody');
+
+                while(table.lastElementChild)
+                {
+                  table.removeChild(table.lastChild);
+                }
+
+                for(let i=0;i<books.length;i+=1)
+                {
+                  const read = books[i].read === true ? 'Read' : 'Yet to Read' ;
+                  let tablerow = document.createElement('tr');
+                  tablerow.setAttribute('id',i);
+                  tablerow.innerHTML = `
+                        <td scope="row">${i+1}</td>
+                        <td>${books[i].book}</td>
+                        <td>${books[i].author}</td>
+                        <td>${books[i].pages}</td>
+                        <td><button class="btn btn-success" id="readbook">${read}</button></td>
+                        <td><button class="btn btn-danger" id="remove">Delete</button></td> 
+
+                        `
+                  table.appendChild(tablerow);
+                }
+      }
+
+      return {displaybooks};
+
+})();
+
+//factory
+
+const displayForm = () => {
   if (bookForm.classList.contains('d-none')) {
     bookForm.classList.remove('d-none');
   } else {
@@ -13,53 +69,26 @@ function displayForm() {
   }
 }
 
-function createBook(book,author,pages,read) {
-  this.book = book;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+
+const createBook = (book,author,pages,read) => {
+return {book,author,pages,read};
 }
 
-function addtoArray(e)
+
+const addtoArray = (e) =>
 { 
   
  e.preventDefault();
-  let book1 = new createBook(book.value,author.value,pages.value,readRadios1.checked)
+  const book1 = createBook(book.value,author.value,pages.value,readRadios1.checked)
   books.push(book1);
   for(let i=0;i<forminput.length;i+=1){
       forminput[i].value='';
       forminput[i].checked=false;
   }
-  displaybooks()
+  moduledisplay.displaybooks();
 }
 
-function displaybooks()
 
-{  const table = document.querySelector('#tbody');
-
-   while(table.lastElementChild)
-  {
-    table.removeChild(table.lastChild);
-  }
-
-  for(let i=0;i<books.length;i+=1)
-  {
-    const read = books[i].read === true ? 'Read' : 'Yet to Read' ;
-    let tablerow = document.createElement('tr');
-    tablerow.setAttribute('id',i);
-    tablerow.innerHTML = `
-          <td scope="row">${i+1}</td>
-          <td>${books[i].book}</td>
-          <td>${books[i].author}</td>
-          <td>${books[i].pages}</td>
-          <td><button class="btn btn-success" id="readbook">${read}</button></td>
-          <td><button class="btn btn-danger" id="remove">Delete</button></td> 
-
-          `
-    table.appendChild(tablerow);
-  }
-
-}
 
 
 document.querySelector('#tbody').addEventListener('click',(event) => {
@@ -89,7 +118,7 @@ if (event.target.id === 'remove')
 
   // event.target.innerHTML === 'Read' ?  event.target.innerHTML = 'Yet to Read' : event.target.innerHTML = 'Read';
     books.splice([e.parentElement.parentElement.id],1);
-  displaybooks();
+  moduledisplay.displaybooks();
 }
 });
 
