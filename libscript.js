5e16a0873578849d23a233f81e7f3b597b9ddecc
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-03 00:31:52 +0530

    Add to localstorage

--- libscript.js
+++ libscript.js
@@ -3,7 +3,16 @@ const submit = document.getElementById('submit');
 const bookForm = document.querySelector('#form-display');
 const formBtn = document.querySelector('#form-btn');
 const forminput = document.querySelectorAll('input');
-let books = []
+// let books = [];
+
+function localstorage(){
+  let books = [];
+
+  if(localStorage.getItem('library')) {
+      books = JSON.parse(localStorage.library);
+  }
+return books
+}
 
 function displayForm() {
   if (bookForm.classList.contains('d-none')) {
@@ -24,7 +33,9 @@ class Createbook {
 
           displaybooks()
 
-          {  const table = document.querySelector('#tbody');
+          {  let books = localstorage(); 
+            
+            const table = document.querySelector('#tbody');
 
             while(table.lastElementChild)
             {
@@ -54,27 +65,30 @@ class Createbook {
 
 
 const addtoArray = (e) => {
-
-  e.preventDefault();
+  let books = localstorage();
+  //  e.preventDefault();
   let book1 = new Createbook(book.value, author.value, pages.value, readRadios1.checked);
   books.push(book1);
-  for (let i = 0; i < forminput.length; i += 1) {
-    forminput[i].value = '';
-    forminput[i].checked = false;
-  }
-  Createbook.prototype.displaybooks();
+  localStorage.setItem("library", JSON.stringify(books));
+
+  // for (let i = 0; i < forminput.length; i += 1) {
+  //   forminput[i].value = '';
+  //   forminput[i].checked = false;
+  // }
+  // Createbook.prototype.displaybooks();
 }
 
 
 
 document.querySelector('#tbody').addEventListener('click',(event) => {
- 
+ let books = localstorage(); 
 if (event.target.id === 'readbook')
 { e = event.target;
 
   // event.target.innerHTML === 'Read' ?  event.target.innerHTML = 'Yet to Read' : event.target.innerHTML = 'Read';
     if (event.target.innerHTML === 'Read') {
     event.target.innerHTML = 'Yet to Read';
+    
     books[e.parentElement.parentElement.id].read = !books[e.parentElement.parentElement.id].read;
   } 
   
@@ -82,18 +96,21 @@ if (event.target.id === 'readbook')
     event.target.innerHTML = 'Read';
     books[e.parentElement.parentElement.id].read = !books[e.parentElement.parentElement.id].read;
   }
+    localStorage.setItem("library", JSON.stringify(books));
+
 }
 });
 
 
 document.querySelector('#tbody').addEventListener('click',(event) => {
- 
+ let books = localstorage(); 
 if (event.target.id === 'remove')
 { e = event.target;
   // console.log(e.parentElement.parentElement.id);
 
   // event.target.innerHTML === 'Read' ?  event.target.innerHTML = 'Yet to Read' : event.target.innerHTML = 'Read';
     books.splice([e.parentElement.parentElement.id],1);
+    localStorage.setItem("library", JSON.stringify(books));
   Createbook.prototype.displaybooks();
 }
 });
@@ -102,3 +119,5 @@ if (event.target.id === 'remove')
 formBtn.addEventListener('click', displayForm);
 
 submit.addEventListener('click',addtoArray);
+
+Createbook.prototype.displaybooks();
